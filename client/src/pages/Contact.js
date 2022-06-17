e2c9c8c39a67235347508493ae59cc831602a3ea
kdobro92 <kdobro92@gmail.com>
2022-06-17 10:23:33 +0900

    [client] 추가: history.js

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,11 +1,9 @@
-import Thumbnail from "../components/Contact/Thumbnail";
 import Content from "../components/Contact/Content";
 import Map from "../components/Contact/Map";
 
 function Contact() {
   return (
     <>
-      {/* <Thumbnail /> */}
       <Content />
       <Map />
     </>
