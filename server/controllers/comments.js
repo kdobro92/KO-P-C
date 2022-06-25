64a5c2d14061ed4dbc27c14d581b47429f74202b
kdobro92 <kdobro92@gmail.com>
2022-06-25 21:58:09 +0900

    [server] 추가 : comment.js

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -5,70 +5,72 @@ dotenv.config();
 
 module.exports = {
   createComment: async (req, res) => {
-    const { commentId } = req.params;
-
+    const { put_deta_cont } = req.body;
+    console.log(put_deta_cont);
+    if (!put_deta_cont) {
+      return res.json({ message: "잘못된 정보 입력" });
+    }
     try {
-      const checkChatContents = await chatcontents.findAll({
-        order: [["createdAt", "DESC"]],
-        where: {
-          chatroomId: chatroomId,
-        },
+      const isCreated = await comments.create({
+        put_deta_cont,
+        board_id,
+        // userId: token.id,
       });
-      return res.status(200).json({ checkChatContents });
+      return res.status(201).json({ data: isCreated, message: "작성 완료" });
     } catch (err) {
-      console.log(err);
       return res.status(500).json({ message: "서버 에러" });
     }
+    // res.status(401).json({ message: "인증 실패" });
   },
 
-  modifyComment: async (req, res) => {
-    const { commentId } = req.params;
+  // modifyComment: async (req, res) => {
+  //   const { commentId } = req.params;
 
-    try {
-      const checkChatContents = await chatcontents.findAll({
-        order: [["createdAt", "DESC"]],
-        where: {
-          chatroomId: chatroomId,
-        },
-      });
-      return res.status(200).json({ checkChatContents });
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({ message: "서버 에러" });
-    }
-  },
+  //   try {
+  //     const checkChatContents = await chatcontents.findAll({
+  //       order: [["createdAt", "DESC"]],
+  //       where: {
+  //         chatroomId: chatroomId,
+  //       },
+  //     });
+  //     return res.status(200).json({ checkChatContents });
+  //   } catch (err) {
+  //     console.log(err);
+  //     return res.status(500).json({ message: "서버 에러" });
+  //   }
+  // },
 
-  createComment: async (req, res) => {
-    const { commentId } = req.params;
+  // createComment: async (req, res) => {
+  //   const { commentId } = req.params;
 
-    try {
-      const checkChatContents = await chatcontents.findAll({
-        order: [["createdAt", "DESC"]],
-        where: {
-          chatroomId: chatroomId,
-        },
-      });
-      return res.status(200).json({ checkChatContents });
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({ message: "서버 에러" });
-    }
-  },
+  //   try {
+  //     const checkChatContents = await chatcontents.findAll({
+  //       order: [["createdAt", "DESC"]],
+  //       where: {
+  //         chatroomId: chatroomId,
+  //       },
+  //     });
+  //     return res.status(200).json({ checkChatContents });
+  //   } catch (err) {
+  //     console.log(err);
+  //     return res.status(500).json({ message: "서버 에러" });
+  //   }
+  // },
 
-  getAllComment: async (req, res) => {
-    const { commentId } = req.params;
+  // getAllComment: async (req, res) => {
+  //   const { commentId } = req.params;
 
-    try {
-      const checkChatContents = await chatcontents.findAll({
-        order: [["createdAt", "DESC"]],
-        where: {
-          chatroomId: chatroomId,
-        },
-      });
-      return res.status(200).json({ checkChatContents });
-    } catch (err) {
-      console.log(err);
-      return res.status(500).json({ message: "서버 에러" });
-    }
-  },
+  //   try {
+  //     const checkChatContents = await chatcontents.findAll({
+  //       order: [["createdAt", "DESC"]],
+  //       where: {
+  //         chatroomId: chatroomId,
+  //       },
+  //     });
+  //     return res.status(200).json({ checkChatContents });
+  //   } catch (err) {
+  //     console.log(err);
+  //     return res.status(500).json({ message: "서버 에러" });
+  //   }
+  // },
 };
--- server/migrations/20220604064407-create-comments.js
+++ server/migrations/20220604064407-create-comments.js
@@ -1,66 +1,75 @@
-'use strict';
+"use strict";
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('comments', {
+    await queryInterface.createTable("comments", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+      },
+      board_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "boards",
+          key: "id",
+        },
+        onDelete: "cascade",
+        onUpdate: "cascade",
       },
       grp_code: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       cust_id: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       put_seri_no: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       seri_no: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       stat_code: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       put_titl_cont: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       put_deta_cont: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       file_text_kind_code: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       file_name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       file_stor_loca_cont: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       firs_crea_date_time: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       firs_crea_empl: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       last_edit_date_time: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       last_edit_empl: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('comments');
-  }
+    await queryInterface.dropTable("comments");
+  },
 };
